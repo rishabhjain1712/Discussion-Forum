64eb0e8faff82d8a436a4095850fe259f4cb2946
rishabhjain1712 <rishabh201224@gmail.com>
2023-12-28 01:26:17 +0530

    first commit

---
+++ client/src/components/common/jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Jumotron = () => {
+  return (
+    <div className="jumbotron">
+      <p className="lead">A disscussion forum</p>
+    </div>
+  );
+};
+
+export default Jumotron;
